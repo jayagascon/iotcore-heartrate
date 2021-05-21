529e6417cb51ed66db0a449426e0b838bd1ba96d
jayagascon <62238235+jayagascon@users.noreply.github.com>
2021-05-21 15:45:57 +1200

    clean up checkHeartRate.py

--- checkHeartRate.py
+++ checkHeartRate.py
@@ -50,15 +50,15 @@ def parse_command_line_args():
             help='GCP cloud project name')
     parser.add_argument(
             '--registry_id', 
-	    required=True, 
-	    help='Cloud IoT Core registry id')
+        required=True,
+        help='Cloud IoT Core registry id')
     parser.add_argument(
             '--device_id', 
-	    required=True, 
-	    help='Cloud IoT Core device id')
+        required=True,
+        help='Cloud IoT Core device id')
     parser.add_argument(
             '--private_key_file',
-	    default='../.ssh/ec_private.pem',
+        default='../.ssh/ec_private.pem',
             help='Path to private key file.')
     parser.add_argument(
             '--algorithm',
@@ -89,7 +89,7 @@ def parse_command_line_args():
     parser.add_argument(
             '--receiver_in',
             default=gpioIN,
-	    type=int,
+        type=int,
             help=('GPIO input pin for the heart rate sensor'))
     return parser.parse_args()
 
@@ -117,10 +117,10 @@ def on_publish(unused_client, unused_userdata, unused_mid):
 
 def createJSON(id, unique_id, timestamp, heartrate):
     data = {
-	'sensorID' : id,
-	'uniqueID' : unique_id,
-	'timecollected' : timestamp,
-	'heartrate' : heartrate
+    'sensorID' : id,
+    'uniqueID' : unique_id,
+    'timecollected' : timestamp,
+    'heartrate' : heartrate
     }
 
     json_str = json.dumps(data)
@@ -147,7 +147,7 @@ def main():
     
     _CLIENT_ID = 'projects/{}/locations/{}/registries/{}/devices/{}'.format(project_id, gcp_location, registry_id, device_id)
     _MQTT_TOPIC = '/devices/{}/events'.format(device_id)
-	
+
     totalSampleCounter = 0 # total heartbeats measured
     sampleCounter = -1 # counter to determine when to calculate an average BPM
     previousInput = 0 # indicator of whether the last sensor input has high or low
@@ -159,7 +159,7 @@ def main():
     uniqueID = -1 # unique id if dedeplication is needed
 
     io.setup(receiver_in, io.IN) # initialize receiver GPIO to the pin that will take input
-    print "Ready. Waiting for signal."
+    print("Ready. Waiting for signal.")
     
     while True:
 
@@ -184,13 +184,13 @@ def main():
       while time.time() < jwt_refresh: # as long as the JWT isn't ready to expire, otherwise break this loop so the JWT gets refreshed
         # Continuously monitor for heart beat signals being received
         try:
-	    inputReceived = io.input(receiver_in) # inputReceived will either be 1 or 0
+            inputReceived = io.input(receiver_in) # inputReceived will either be 1 or 0
 
             if inputReceived == 1:
                 if previousInput == 0: # the heart beat signal went from low to high
                     totalSampleCounter = totalSampleCounter + 1
                     if totalSampleCounter == 1: # the very first beat since the program started running
-                        print "Receiving heart beat signals"
+                        print("Receiving heart beat signals")
                 
                     if sampleCounter == -1: # the first beat received since the counter was reset
                         sampleCounter = 0
@@ -210,17 +210,17 @@ def main():
                         lastSampleTime = lastPulseTime # set the time the last beat was detected
                         bpm = calcBPM(firstSampleTime, lastSampleTime)
                         currentTime = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-			uniqueID = str(uuid.uuid4()) + "-" + sensorID
+                        uniqueID = str(uuid.uuid4()) + "-" + sensorID
                         payload = createJSON(sensorID, uniqueID, currentTime, bpm)
                         client.publish(_MQTT_TOPIC, payload, qos=1)
                         print("{}\n".format(payload))
                         time.sleep(0.5)
             previousInput = inputReceived
         except Exception as e:
-	  print "There was an error"
-	  print (e)
-		
+            print("There was an error")
+            print (e)
+
       client.loop_stop()
 
 if __name__ == '__main__':
-	main()
+    main()
